aed9990aacea862d930a9bc6c408e5053efc5bc4
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-07-11 14:30:13 -0500

    Object values/Arrays and objects

--- Javascript/script.js
+++ Javascript/script.js
@@ -19,3 +19,53 @@ const person = {
 //Getting data out of objects
 person['firstName'] //prints out 'Mark'
 fitBitData.totalMiles //print out 211.9
+
+//changing the key value of a property
+person.firstName = 'John';
+//adding a property and a key value
+person.age = 40;
+person.sex = 'M';
+
+console.log(person.sex);
+
+//Nesting arrays in objects
+//Array of objects
+//Or objects with properties whose key values are arrays
+
+//Array of objects
+const shoppingCart = [
+{
+    product: 'Eggs',
+    price: 1.20,
+    quantity: 1,
+},
+{
+    product: 'Ham',
+    price: 20,
+    quantity: 5,
+},
+{
+    product: 'Milk',
+    price: 2.20,
+    quantity: 2
+}
+]
+
+//Object containing arrays
+const student = {
+    firstName: 'Luis',
+    lastName: 'Jones',
+    favColors: ['blue','red','brown'],
+    //Nesting an object inside an object
+    exams: {midterm: 92, final: 88}
+}
+
+const comments = [
+    {username: 'Tam', text: 'lol', likes: 32},
+    {username: 'John', text: 'xoxo', likes: 3423}
+]
+
+//Accessing first comment:
+comments[1]
+//Accessing text from first comment
+comments[1].text //'lol'
