5334a443ffac36fd0ff81994b0f978ba7ddbbf0b
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-07-19 21:47:53 -0500

    onclick and onmouseenter in script

--- Javascript/Events/script.js
+++ Javascript/Events/script.js
@@ -2,3 +2,15 @@
 function func(){
     alert("Don't click on the button!!");
 }
+
+const btn = document.querySelector('#v2');
+btn.onclick = function(){
+    alert('CLICKED!');
+}
+
+function scream(){
+    alert('mouse enter!!')
+}
+//mouse enter
+//setting a property to be a function 
+btn.onmouseenter = scream;
