21f9a1b46eddab68dfc74b154bfab67674366ec2
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-07-20 00:03:26 -0500

    addEventListener

--- Javascript/Events/script.js
+++ Javascript/Events/script.js
@@ -14,3 +14,16 @@ function scream(){
 //mouse enter
 //setting a property to be a function 
 btn.onmouseenter = scream;
+//addEventListener: specify the event type and a callback to run
+// const button = document.querySelector('h1');
+// //'use a generic method'
+// button.addEventListener('click', () => {
+//     alert('YOU CLICKED!');
+// })
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('dblclick', function() {
+    alert('clicked v3');
+})
+//once: true, invokes at most once after being added
+btn3.addEventListener('click', func, {once: true});
