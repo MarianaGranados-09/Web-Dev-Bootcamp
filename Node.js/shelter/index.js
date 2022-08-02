6dcf47f9f644c26618122badd9e08c2bc63558be
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-08-02 15:52:36 -0500

    Requiring a directory

---
+++ Node.js/shelter/index.js
@@ -0,0 +1,12 @@
+const blue = require('./blue');
+const sadie = require('./sadie');
+const janet = require('./janet');
+
+const allCats = [blue, sadie, janet];
+console.log(allCats);
+
+module.exports = allCats;
+
+//requiring a directory
+//const cats = require('./shelter)
+//console.log(cats)
