e2ab04c8fbc42ece85d9796c8353c3a7bdb067c1
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-08-03 23:42:15 -0500

    .get and .post method

--- Express.js/index.js
+++ Express.js/index.js
@@ -7,15 +7,35 @@ const app = express()
 //two objects automatically passed in:
 //-represents the request
 //-represents the response
-app.use((req, res) => {
-    console.log('NEW REQUEST');
-    // console.dir(req)7
-    //res.send is going to send and generate an HTTP response 
-    res.send('<h1>This is my page!</h1>');
-    //sending an object through http:
-    // res.send({color: 'red'})
-})
+// app.use((req, res) => {
+//     console.log('NEW REQUEST');
+//     // console.dir(req)7
+//     //res.send is going to send and generate an HTTP response 
+//     res.send('<h1>This is my page!</h1>');
+//     //sending an object through http:
+//     // res.send({color: 'red'})
+// })
+
 //adding port to listen to
 app.listen(3000, () => {
     console.log('Listening port 3000');
+})
+
+//routing: taking incoming requests and a path that is requested
+//and matching that to some code in some response: 
+// /cats = 'meow'
+// /dogs = 'woof'
+
+//.get method expects two things: paths we are matching and callback function
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW');
+})
+app.get('/dogs', (req, res) => {
+    res.send('WOOF');
+})
+
+//.post method 
+app.post('/cats', (req, res) => {
+    res.send('POST MEOW');
 })
