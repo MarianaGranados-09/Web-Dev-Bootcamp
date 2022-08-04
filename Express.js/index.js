99f884443c0250b20d0694883a46fb20d38f2fab
Mariana Granados <89766019+MarianaGranados-09@users.noreply.github.com>
2022-08-03 22:25:51 -0500

    request and response objects

--- Express.js/index.js
+++ Express.js/index.js
@@ -4,8 +4,16 @@ const app = express()
 //console.dir(app)
 
 //Everytime we have an incoming request, this method will run
-app.use(() => {
+//two objects automatically passed in:
+//-represents the request
+//-represents the response
+app.use((req, res) => {
     console.log('NEW REQUEST');
+    // console.dir(req)7
+    //res.send is going to send and generate an HTTP response 
+    res.send('<h1>This is my page!</h1>');
+    //sending an object through http:
+    // res.send({color: 'red'})
 })
 //adding port to listen to
 app.listen(3000, () => {
