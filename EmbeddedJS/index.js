a564b02792157d0029428354ad0acf81ca1696f3
Mariana Granados <mgranados09@alumnos.uaq.mx>
2022-10-11 15:37:09 -0500

    app.get app.set

--- EmbeddedJS/index.js
+++ EmbeddedJS/index.js
@@ -7,6 +7,16 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => { 
     res.render('home.ejs');
 })
+app.get('/rand', (req, res) => {
+    //Generate number here
+    let numRand = Math.floor(Math.random() * 10) +1;
+    res.render('random', {rand: numRand})
+})
+app.get('/r/:subreddit', (req, res ) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', {subreddit});
+})
+//http://localhost:3000/r/dogs
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
